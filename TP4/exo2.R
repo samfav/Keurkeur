3035839f087884eac58d9fdce6c0cb849b3b08c5
MarieChatelin <marie.chatelin@hotmail.fr>
2015-06-01 22:52:18 +0200

    Begin of 2.1.2

--- TP4/exo2.R
+++ TP4/exo2.R
@@ -22,6 +22,8 @@ calcul_gradient <- function(beta, Xapp, t_cout) {
 	return (result)
 }
 
+#2.1.1
+
 calcul_log <- function(beta, Xapp, t_cout) {
 	n = length(t_cout)
 	nbDimension <- dim(Xapp)[2]
@@ -53,3 +55,28 @@ log.app <- function(Xapp, zapp, intr, epsi) {
 	return(beta)
 }
 
+#2.1.2
+La fonction log.val, permettant d’évaluer un ensemble de test, prendra comme arguments d’entrée le tableau de données Xtst à classer et la matrice beta des paramètres déterminés par la fonction log.app. Cette fonction fera un test sur les dimensions de la matrice beta, pour déterminer si une ordonnée à l’origine doit être ou non ajoutée à la matrice d’exemples Xtst à évaluer. Elle devra retourner une structure contenant la matrice prob des probabilités a posteriori estimées et le vecteur des classements associés. Ces fonctions pourront s’appuyer sur une fonction calculant les probabilités a posteriori à partir d’une matrice de paramètres beta et d’un tableau de données X.
+
+log.val <- function(Xtst, beta) {
+	result = matrix(sample(0, nbDimension, nbDimension + 1, replace = TRUE), nbDimension, nbDimension + 1)
+	proba = matrix(sample(0, nbDimension, nbDimension, replace = TRUE), nbDimension, nbDimension)
+	nbDimension = dim(Xtst)[2]
+	n = dim(Xtst)[1]
+	ordonnee = dim(beta)[1]
+	
+	if (ordonnee != n) {
+		Xtst = ajout_ordonnee(Xtst, ordonnee)
+	}
+	
+	for (i in 1:n) {
+		for (j in 1:nbDimension) {
+			proba[i][j] = exp(t(beta) * Xtst[i][j]) / (exp(t(beta) * Xtst[i][j]) + 1)
+		}
+	}
+	
+	return (result)
+}
+
+#2.1.3
+Il est possible de généraliser le modèle de régression logistique de manière très simple. La stratégie consiste à transformer les données dans un espace plus complexe, dans lequel les classes peuvent être séparées par un hyperplan. La régression logistique est alors effectuée dans cet espace. Par exemple, dans le cas où les individus sont décrits par les variables X1 , X2 et X3 , la régression logistique quadratique consiste à effectuer la régression logistique classique dans l’espace correspondant aux variables X1 , X2 , X3 , X1X2 , X1X3 , X2X3 , (X1 ) 2 , (X2 ) 2 et (X3 ) 2 , que l’on notera X 2 , plutôt que dans l’espace X = {X1 , X2 , X3}. Le modèle ainsi défini est donc plus flexible ; mais le nombre de paramètres à estimer étant plus important (p(p+ 3)/2 au lieu de p), il peut également s’avérer moins robuste que le modèle classique déterminé dans l’espace des caractéristiques initiales.
